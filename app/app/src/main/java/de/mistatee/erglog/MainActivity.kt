3e746cec0141e131fe08dc46f2c1bcb5c4e6e977
Tobias Schröpf <tobias.schroepf@jambit.com>
2026-08-07 08:11:58 +0200

    Initial project setup

---
+++ app/app/src/main/java/de/mistatee/erglog/MainActivity.kt
@@ -0,0 +1,22 @@
+package de.mistatee.erglog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import de.mistatee.erglog.theme.ErgLogTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      ErgLogTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
+    }
+  }
+}
